b6944fb300e22b9cdf82a8ad6f71bc0359ed7652
yudit12 <yudit12@users.noreply.github.com>
2020-03-12 12:58:18 +0200

    Updates

---
+++ precache-manifest.84723367d772ea18d74b4a25666159f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cc4efd50318da643da2946e331ae11",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "19884cd71ebbe5f70fdf",
+    "url": "/face-recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fb2b77f826956c400895",
+    "url": "/face-recognition/static/css/main.ff606427.chunk.css"
+  },
+  {
+    "revision": "19884cd71ebbe5f70fdf",
+    "url": "/face-recognition/static/js/2.78395182.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/face-recognition/static/js/2.78395182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb2b77f826956c400895",
+    "url": "/face-recognition/static/js/main.d4c32206.chunk.js"
+  },
+  {
+    "revision": "4c6dfc7cf8eb2f846773",
+    "url": "/face-recognition/static/js/runtime-main.17122dc1.js"
+  },
+  {
+    "revision": "f7fd504d5bf1a035fd757c8d195d9f34",
+    "url": "/face-recognition/static/media/brain.f7fd504d.png"
+  }
+]);
